eafc702f730dd83169f68f40d82501b6b99e40a7
segfault0x61 <torresj1997@outlook.com>
2023-09-08 12:27:43 -0500

    Add Unit Tests for Domain Services

---
+++ test/TodoList.Domain.UnitTests/TodoItems/PrioritySuggestionServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using TodoList.Domain.TodoItems;
+
+namespace TodoList.Domain.UnitTests.TodoItems;
+
+public class PrioritySuggestionServiceTests
+{
+    [Theory]
+    [InlineData(5, "Low")]
+    [InlineData(0, "High")]
+    [InlineData(1, "High")]
+    [InlineData(2, "Medium")]
+    public void SuggestPriority_ShouldReturnExpectedPriority(int daysUntilDue, string expectedPriority)
+    {
+        // Arrange
+        var service = new PrioritySuggestionService();
+        var dueDate = DateOnly.FromDateTime(DateTime.UtcNow.AddDays(daysUntilDue));
+        var todo = new TodoItem("Test task", dueDate, service);
+
+        // Act
+        var priority = service.SuggestPriority(todo);
+
+        // Assert
+        Assert.Equal(expectedPriority, priority);
+    }
+}
