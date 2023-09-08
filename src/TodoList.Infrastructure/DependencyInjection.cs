6124f5b1803264036472cc545eb7b5f84e17a84b
segfault0x61 <torresj1997@outlook.com>
2023-09-08 11:34:12 -0500

    Register repositories based on naming conventions using reflection

--- src/TodoList.Infrastructure/DependencyInjection.cs
+++ src/TodoList.Infrastructure/DependencyInjection.cs
@@ -1,10 +1,9 @@
-﻿using System.Configuration;
+﻿using System.Reflection;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TodoList.Application.Context;
 using TodoList.Domain.Abstractions;
-using TodoList.Domain.TodoItems;
 using TodoList.Infrastructure.Data;
 using TodoList.Infrastructure.Repositories;
 
@@ -14,16 +13,27 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = 
+        var connectionString =
             configuration.GetConnectionString("DefaultConnection") ??
             throw new ArgumentNullException(nameof(configuration));
 
-        services.AddScoped<ITodoItemRepository, TodoItemRepository>();
+        // Register repositories using reflection based on naming convention
+        var repositoryTypes = Assembly.GetAssembly(typeof(TodoItemRepository)) // Assuming TodoItemRepository is representative of the assembly containing all repositories
+            ?.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.Name.EndsWith("Repository"))
+            .ToList();
+
+        foreach (var type in repositoryTypes)
+        {
+            var interfaceType = type.GetInterfaces().FirstOrDefault(i => i.Name == "I" + type.Name);
+            if (interfaceType != null)
+            {
+                services.AddScoped(interfaceType, type);
+            }
+        }
 
         services.AddTransient<IUnitOfWork, DapperUnitOfWork>(c => new DapperUnitOfWork(connectionString));
-
         services.AddSingleton(new MySqlConnectionFactory(connectionString));
-
         SqlMapper.AddTypeHandler(new DateOnlyHandler());
 
         return services;
