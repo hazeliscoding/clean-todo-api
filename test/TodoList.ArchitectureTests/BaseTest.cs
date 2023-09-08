460d61fa5547223772bc04866690b4c7ebcfdb46
segfault0x61 <torresj1997@outlook.com>
2023-09-08 11:58:44 -0500

    Add Architecture Tests

---
+++ test/TodoList.ArchitectureTests/BaseTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using TodoList.Application.Context;
+using TodoList.Domain.Abstractions;
+using TodoList.Infrastructure.Data;
+
+namespace TodoList.ArchitectureTests;
+
+public class BaseTest
+{
+    protected static Assembly ApplicationAssembly => typeof(DapperUnitOfWork).Assembly;
+    protected static Assembly DomainAssembly => typeof(BaseEntity).Assembly;
+    protected static Assembly InfrastructureAssembly => typeof(MySqlConnectionFactory).Assembly;
+}
