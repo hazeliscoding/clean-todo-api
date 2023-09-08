c4f30326269a4a500f75f1f6143ddb83c85ccaa0
segfault0x61 <torresj1997@outlook.com>
2023-09-08 13:32:14 -0500

    Add Application Unit Tests

---
+++ test/TodoList.Application.UnitTests/TodoItems/Commands/CreateTodoItemCommandHandlerTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using TodoList.Application.TodoItems.CreateTodoItem;
+using TodoList.Domain.Abstractions;
+using TodoList.Domain.TodoItems;
+using TodoList.Infrastructure.Repositories;
+
+namespace TodoList.Application.UnitTests.TodoItems.Commands;
+
+public class CreateTodoItemCommandHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldPersistTodoItem()
+    {
+        // Arrange
+        var mockUnitOfWork = new Mock<IUnitOfWork>();
+        var mockRepository = new Mock<ITodoItemRepository>();
+
+        mockRepository.Setup(r => r.AddAsync(It.IsAny<TodoItem>())).Returns(Task.CompletedTask);
+
+        // You can either use a real PrioritySuggestionService or mock it.
+        // If you just want to test the handler logic, it's often easier to use the real service if it doesn't have external dependencies.
+        var priorityService = new PrioritySuggestionService();
+
+        var handler = new CreateTodoItemCommandHandler(mockRepository.Object, mockUnitOfWork.Object, priorityService);
+        var command = new CreateTodoItemCommand
+        {
+            Title = "Test Task",
+            DueDate = DateOnly.MinValue
+        };
+
+        // Act
+        await handler.Handle(command, CancellationToken.None);
+
+        // Assert
+        mockRepository.Verify(r => r.AddAsync(It.IsAny<TodoItem>()), Times.Once());
+        mockUnitOfWork.Verify(u => u.Commit(), Times.Once());
+    }
+}
---
+++ test/TodoList.Application.UnitTests/TodoItems/Queries/GetTodoItemByIdQueryHandlerTests.cs
@@ -0,0 +1,44 @@
+﻿using Moq;
+using TodoList.Application.TodoItems;
+using TodoList.Application.TodoItems.GetTodoItem;
+using TodoList.Domain.TodoItems;
+
+namespace TodoList.Application.UnitTests.TodoItems.Queries;
+
+public class GetTodoItemByIdQueryHandlerTests
+{
+    [Fact]
+    public async Task Handle_ShouldReturnCorrectTodoItem()
+    {
+        // Arrange
+        var todoItem = new TodoItem("Test", DateOnly.MinValue, new PrioritySuggestionService());
+        var todoItemDto = new TodoItemResponse
+        {
+            Id = todoItem.Id,
+            Title = todoItem.Title,
+            DueDate = todoItem.DueDate,
+            IsCompleted = todoItem.IsCompleted,
+            Priority = todoItem.Priority
+        }; 
+
+        var mockRepository = new Mock<ITodoItemRepository>();
+
+        mockRepository.Setup(r => r.GetByIdAsync(It.IsAny<int>())).ReturnsAsync(todoItem);
+
+        var handler = new GetTodoItemByIdQueryHandler(mockRepository.Object);
+
+        var query = new GetTodoItemByIdQuery
+        {
+            Id = 1
+        };
+
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
+
+        // Assert
+        Assert.Equal(todoItemDto.Title, result.Title);
+        Assert.Equal(todoItemDto.DueDate, result.DueDate);
+        Assert.Equal(todoItemDto.IsCompleted, result.IsCompleted);
+        Assert.Equal(todoItemDto.Priority, result.Priority);
+    }
+}
